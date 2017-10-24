9c84da2e273527d8a05d4876281b52230aa36332
Normsmiway <norms.miway@gmail.com>
2017-10-24 18:29:32 +0100

    site update

---
+++ Sfais.web/Code/Converter.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Generic;
+namespace SfaisWeb.Code
+{
+    public static class Converter
+    {
+        public static string[] JsToCSharp(string JsObject)
+        {
+            List<string> step2Result = new List<string>();
+            //Check the string , plits by }
+            //from each object, remove starting ,
+            var step1Result = JsObject.Split(new char[] { '}' });
+
+            foreach (string data  in step1Result)
+            {
+               var d= data.TrimStart(new char[] { '{', ',' }).Replace(':','=').Replace('\'','"').Trim(new char[] {'{' });
+                step2Result.Add(d);
+            }
+            var m = step2Result.ToString();
+            return step1Result;
+
+        }
+    }
+}
---
+++ Sfais.web/Controllers/AppController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sfais.Web.Controllers
+{
+    
+    public class AppController : Controller
+    {
+        
+        [Route("")]
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+       
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+    }
+}
